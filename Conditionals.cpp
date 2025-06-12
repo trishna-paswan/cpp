1e2c2be9cfcb3fb6872900a904dc8ea4f6492743
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-12 22:28:58 +0530

    Create Conditionals.cpp

---
+++ Conditionals.cpp
@@ -0,0 +1,22 @@
+// CONDITIONALS
+#include <iostream>
+using namespace std;
+
+int main(){
+    int rating = 4;
+    if(rating == 5){
+        puts("5 star rated.");
+    }
+    else if(rating == 4){
+        puts("4 star");
+    } else {
+        puts("Not 5 star rated.");
+    }
+
+    if (1){
+        puts("Go for it.");
+    }
+
+    printf("Your rating feedback is %s\n", rating == 4 ? "true block" : "false block");
+    return 0;
+}
