bdd5e2b9dc831aa95ff80a46f0e55285eef40f62
Trishna Paswan <S24CSEU0083@bennett.edu.in>
2025-06-12 22:27:34 +0530

    Create Pointers_Reference.cpp

---
+++ Pointers_Reference.cpp
@@ -0,0 +1,18 @@
+// POINTERS VS REFERENCES
+#include <iostream>
+using namespace std;
+
+int main(){
+    int score = 200;
+    int *myp = &score;
+
+    printf("Value of score is %d \n", score);
+    printf("Value for pointer is %p\n", myp);
+
+    int &another_score = score;
+    another_score = 800;
+
+    printf("Value of score is %d \n", score);
+    printf("Value for pointer is %p\n", myp);
+    return 0;
+}
